0a673a9674092dc48ce522ce0dcdeb85681851ac
Lenovo <orvinoray@gmail.com>
2025-07-31 23:45:31 +0700

    buat migrasi buku & routing buku

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('books')->group(function(){
+    Route::get('/',[BookController::class, 'index'])->name('buku.index');
+    Route::get('/create', [BookController::class, 'create'])->name ('buku.create');
+    Route::post('/', [BookController::class, 'store'])->name('buku.store');
+    Route::get('/{id}/edit', [BookController::class, 'edit'])->name('buku.edit');
+    Route::put('/{id}', [BookController::class, 'update'])->name('buku.update');
+    Route::delete('/{id}', [BookController::class, 'destroy'])->name('buku.destroy');
+    Route::get('/{id}', [BookController::class, 'show'])->name('buku.show');
 });
